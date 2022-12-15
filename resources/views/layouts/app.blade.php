8ec21a35ea05156f728f9179b54b7dd08addd33f
verplancken <42614785+verplancken@users.noreply.github.com>
2022-12-14 18:57:57 -0600

    cambio de url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,7 +81,7 @@
             <div class="row align-items-center justify-content-lg-between">
               <div class="col-lg-6">
                 <div class="copyright text-center text-lg-left text-muted">
-                  &copy; 2022 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Power By WebTech</a>
+                  &copy; 2022 <a href="https://www.instagram.com/paradisus_spa/" class="font-weight-bold ml-1" target="_blank">Power By Paradisus</a>
                 </div>
               </div>
             </div>
