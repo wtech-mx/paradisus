6796db994b98fddd1fafd9ada8931c96c4f852bc
verplancken <42614785+verplancken@users.noreply.github.com>
2023-01-26 15:46:01 -0600

    show nota

--- resources/views/notas/show.blade.php
+++ resources/views/notas/show.blade.php
@@ -30,6 +30,17 @@
                         <i class="ni ni-credit-card text-sm me-2"></i> Pago
                     </a>
                 </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link mb-0 px-0 py-1" data-bs-toggle="tab" href="#show-sesion{{$notas->id}}" role="tab" aria-controls="pills-sesion" aria-selected="true" id="pills-sesion-tab">
+                        <i class="fa fa-calendar-day text-sm me-2"></i> Sesiones
+                    </a>
+
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link mb-0 px-0 py-1" data-bs-toggle="tab" href="#">
+                        <i class="fa fa-money-bill text-sm me-2"></i> Extras
+                    </a>
+                </li>
             </ul>
 
             <div class="modal-body">
@@ -90,9 +101,8 @@
 
                         <div class="row text-center">
                             <div class="col-2" style="background-color: #bb546c; color: #fff;">Fecha</div>
-                            <div class="col-2" style="background-color: #bb546c; color: #fff;">Cosme</div>
+                            <div class="col-3" style="background-color: #bb546c; color: #fff;">Cosme</div>
                             <div class="col-2" style="background-color: #bb546c; color: #fff;">Pago</div>
-                            <div class="col-1" style="background-color: #bb546c; color: #fff;">Sesión</div>
                             <div class="col-2" style="background-color: #bb546c; color: #fff;">Metodo </div>
                             <div class="col-2" style="background-color: #bb546c; color: #fff;">Nota</div>
                             <div class="col-1" style="background-color: #bb546c; color: #fff;">Foto</div>
@@ -107,17 +117,13 @@
                                     value="{{$item->fecha}}" disabled>
                             </div>
 
-                            <div class="col-2 py-2" ><input name="cosmetologa" type="text" class="form-control text-center" id="cosmetologa"
+                            <div class="col-3 py-2" ><input name="cosmetologa" type="text" class="form-control text-center" id="cosmetologa"
                                     value="{{$item->cosmetologa}}" disabled>
                             </div>
 
                             <div class="col-2 py-2" >
                                 <input name="pago" type="number" class="form-control text-center" id="pago"
                                     value="{{$item->pago}}" disabled></div>
-
-                            <div class="col-1 py-2" ><input name="num_sesion" type="number" class="form-control text-center" id="num_sesion"
-                                value="{{$item->num_sesion}}" disabled>
-                            </div>
 
                             <div class="col-2 py-2" ><input name="forma_pago" type="text" class="form-control text-center" id="forma_pago"
                                 value="{{$item->forma_pago}}" disabled>
@@ -145,6 +151,31 @@
                         </div>
                     </div>
 
+                    <div class="tab-pane fade" id="show-sesion{{$notas->id}}" role="tabpanel" aria-labelledby="pills-sesion-tab">
+                        @foreach ($notas_sesiones as $item)
+                            @if ($item->id_nota == $notas->id)
+                                <div class="row">
+                                    <div class="col-3">
+                                        <div class="form-group">
+                                            <label for="fecha">Fecha</label>
+                                            <input  id="fecha_sesion" name="fecha_sesion" type="date" class="form-control" value="{{$item->fecha}}" disabled>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-2">
+                                        <div class="form-group">
+                                            <label for="num_sesion">Num sesion</label>
+                                            <input  id="sesion" name="sesion" type="number" class="form-control" value="{{$item->sesion}}" disabled>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+
+                    <div class="tab-pane fade" id="pills-extra{{$notas->id}}" role="tabpanel" aria-labelledby="pills-extra-tab">
+                        <h3>Extra</h3>
+                    </div>
                 </div>
             </div>
         </div>
