01de0c81e94f903bd7ccf48f9a74dbd42c04ffe9
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-03-02 08:52:11 -0600

    Update firma_1.blade.php

--- resources/views/paquetes_servicios/paquete_1/firma_1.blade.php
+++ resources/views/paquetes_servicios/paquete_1/firma_1.blade.php
@@ -1,16 +1,7 @@
-
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css">
-
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<link type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/south-street/jquery-ui.css" rel="stylesheet">
-
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-
-<script type="text/javascript" src="http://keith-wood.name/js/jquery.signature.js"></script>
-{{-- <script type="text/javascript" href="{{ asset('assets/js/jquery.signature.js')}}"></script> --}}
-
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/jquery.signature.css')}}">
-{{-- <link rel="stylesheet" type="text/css" href="http://keith-wood.name/css/jquery.signature.css"> --}}
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<link type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/south-street/jquery-ui.css" rel="stylesheet">
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
 <style>
     .kbw-signature { width: 100%; height: 200px;}
@@ -986,12 +977,8 @@
     </div>
   </main>
 
-  {{-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> --}}
-{{-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<script type="text/javascript" href="{{ asset('assets/js/jquery.signature.js')}}"></script> --}}
-{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> --}}
-
-
+  <script type="text/javascript" src="http://keith-wood.name/js/jquery.signature.js"></script>
+  <link rel="stylesheet" type="text/css" href="http://keith-wood.name/css/jquery.signature.css">
   <script type="text/javascript">
 
       var sig = $('#sig').signature({syncField: '#signed', syncFormat: 'PNG'});
