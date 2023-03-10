f80ecf47bb9a88c6d0cb7a00aa115034cff041bf
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-03-10 07:58:58 -0600

    Actualizacion adaptativa y fixed bar

--- resources/views/paquetes_servicios/paquete_4/firma_1.blade.php
+++ resources/views/paquetes_servicios/paquete_4/firma_1.blade.php
@@ -13,7 +13,71 @@
 <style>
     .kbw-signature { width: 100%; height: 200px;}
     #sig canvas{ width: 100% !important; height: auto;}
+
+    .tab-pane{
+        padding: 15px 15px 15px 15px;
+    }
+    .custom_col{
+
+    }
+    .icon-bar {
+  position: fixed;
+  top: 50%;
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+  z-index: 10;
+  right: 0;
+
+}
+
+.icon-bar a {
+  display: block;
+  text-align: center;
+  padding: 16px;
+  transition: all 0.3s ease;
+  color: white;
+  font-size: 20px;
+}
+
+.icon-bar a:hover {
+  background-color: #000;
+}
+.content {
+  margin-left: 75px;
+  font-size: 30px;
+}
+
+.facebook {
+  background: #D7819D;
+  color: white;
+}
 </style>
+
+<div class="icon-bar">
+    <a href="#nav-uno" class="facebook">
+        1
+    </a>
+
+    <a href="#nav-dos" class="facebook">
+        2
+    </a>
+
+    <a href="#nav-tres" class="facebook">
+        3
+    </a>
+
+    <a href="#nav-cuatro" class="facebook">
+        4
+    </a>
+
+    <a href="#nav-ciento" class="facebook">
+        5
+    </a>
+    <a href="#nav-ciento" class="facebook">
+        6
+    </a>
+  </div>
 
 <main class="main-content main-content-bg mt-0">
     <div class="page-header min-vh-100" style="background-image: url('https://img.freepik.com/foto-gratis/composicion-spa-articulos-cuidado-corporal-luz_169016-4162.jpg?w=1380&t=st=1673891361~exp=1673891961~hmac=c4906a893a92ac918c550c104ff3007b935c475c8b53275e4b26bebee610d48f');">
