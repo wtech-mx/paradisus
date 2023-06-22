83cd92efd7be4a2fdd0a91b2770699e67ac9afad
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2023-06-22 14:56:31 -0600

    Correcion de ruta

--- routes/web.php
+++ routes/web.php
@@ -102,7 +102,7 @@ Route::group(['middleware' => ['auth']], function() {
     Route::get('/servicios', [App\Http\Controllers\ServiciosController::class, 'index'])->name('servicio.index');
     Route::post('/servicios/create', [App\Http\Controllers\ServiciosController::class, 'store'])->name('servicio.store');
     Route::patch('/servicios/update/{id}', [App\Http\Controllers\ServiciosController::class, 'update'])->name('servicio.update');
-    Route::patch('/servicios/update/{id}', [App\Http\Controllers\ServiciosController::class, 'update_image'])->name('servicio.update_image');
+    Route::patch('/servicios/update_img/{id}', [App\Http\Controllers\ServiciosController::class, 'update_image'])->name('servicio.update_image');
     Route::delete('/servicios/delete/{id}', [App\Http\Controllers\ServiciosController::class, 'destroy'])->name('servicio.destroy');
 
     Route::get('changeStatus', [App\Http\Controllers\ServiciosController::class, 'ChangeServicioStatus'])->name('ChangeServicioStatus.servicio');
