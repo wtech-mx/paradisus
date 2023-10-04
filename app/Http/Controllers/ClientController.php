b06105e3ce201c9576f1ff74d3ff391630d66f73
verplancken <42614785+verplancken@users.noreply.github.com>
2023-10-04 15:59:14 -0600

    borrar users

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -15,6 +15,16 @@ use Session;
 use DB;
 use DataTables;
 Use Alert;
+use App\Models\Alertas;
+use App\Models\NotasCosmes;
+use App\Models\NotasPedidos;
+use App\Models\NotasPropinas;
+use App\Models\Pagos;
+use App\Models\Paquete2;
+use App\Models\Paquete3;
+use App\Models\Paquetes;
+use App\Models\PaquetesPago;
+use App\Models\User;
 
 /**
  * Class ClientController
@@ -30,6 +40,168 @@ class ClientController extends Controller
     public function index(Request $request)
     {
         $clients = Client::orderBy('id','DESC')->get();
+
+        // IDs de los usuarios que deseas eliminar
+        $usuarios_a_eliminar = [2, 18, 19, 17, 15, 10, 7];
+
+        // ID del usuario al que deseas reasignar las notas
+        $usuario_reemplazo_id = 14; // Cambia esto al ID del usuario al que deseas reasignar las notas
+
+        foreach ($usuarios_a_eliminar as $usuario_id) {
+            // Obtén el usuario que deseas eliminar
+            $usuario_a_eliminar = User::find($usuario_id);
+
+            // Obtén todas las notas relacionadas con ese usuario
+            $notas_relacionadas = NotasCosmes::where('id_user', $usuario_id)->get();
+
+            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+            foreach ($notas_relacionadas as $nota) {
+                $nota->id_user = $usuario_reemplazo_id;
+                $nota->save();
+            }
+
+            // Obtén todas las notas relacionadas con ese usuario
+            $notas_relacionadas2 = NotasPedidos::where('id_user', $usuario_id)->get();
+
+            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+            foreach ($notas_relacionadas2 as $nota) {
+                $nota->id_user = $usuario_reemplazo_id;
+                $nota->save();
+            }
+
+            // Obtén todas las notas relacionadas con ese usuario
+            $notas_relacionadas3 = NotasPropinas::where('id_user', $usuario_id)->get();
+
+            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+            foreach ($notas_relacionadas3 as $nota) {
+                $nota->id_user = $usuario_reemplazo_id;
+                $nota->save();
+            }
+
+            // Obtén todas las notas relacionadas con ese usuario
+            $notas_relacionadas4 = Pagos::where('cosmetologa', $usuario_id)->get();
+
+            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+            foreach ($notas_relacionadas4 as $nota) {
+                $nota->cosmetologa = $usuario_reemplazo_id;
+                $nota->save();
+            }
+
+            // Obtén todas las notas relacionadas con ese usuario
+            $notas_relacionadas5 = PaquetesPago::where('id_user', $usuario_id)->get();
+
+            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+            foreach ($notas_relacionadas5 as $nota) {
+                $nota->id_user = $usuario_reemplazo_id;
+                $nota->save();
+            }
+
+            // Obtén todas las notas relacionadas con ese usuario
+            $notas_relacionadas6 = Paquetes::where('id_user1', $usuario_id)->get();
+
+            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+            foreach ($notas_relacionadas6 as $nota) {
+                $nota->id_user1 = $usuario_reemplazo_id;
+                $nota->save();
+            }
+
+                // Obtén todas las notas relacionadas con ese usuario
+                $notas_relacionadas61 = Paquetes::where('id_user2', $usuario_id)->get();
+
+                // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+                foreach ($notas_relacionadas61 as $nota) {
+                    $nota->id_user2 = $usuario_reemplazo_id;
+                    $nota->save();
+                }
+
+                            // Obtén todas las notas relacionadas con ese usuario
+                            $notas_relacionadas62 = Paquetes::where('id_user3', $usuario_id)->get();
+
+                            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+                            foreach ($notas_relacionadas62 as $nota) {
+                                $nota->id_user3 = $usuario_reemplazo_id;
+                                $nota->save();
+                            }
+
+                                        // Obtén todas las notas relacionadas con ese usuario
+                                        $notas_relacionadas63 = Paquetes::where('id_user4', $usuario_id)->get();
+
+                                        // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+                                        foreach ($notas_relacionadas63 as $nota) {
+                                            $nota->id_user4 = $usuario_reemplazo_id;
+                                            $nota->save();
+                                        }
+
+
+            // Obtén todas las notas relacionadas con ese usuario
+            $notas_relacionadas7 = Paquete2::where('id_user5', $usuario_id)->get();
+
+            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+            foreach ($notas_relacionadas7 as $nota) {
+                $nota->id_user5 = $usuario_reemplazo_id;
+                $nota->save();
+            }
+
+                // Obtén todas las notas relacionadas con ese usuario
+                $notas_relacionadas71 = Paquete2::where('id_user6', $usuario_id)->get();
+
+                // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+                foreach ($notas_relacionadas71 as $nota) {
+                    $nota->id_user6 = $usuario_reemplazo_id;
+                    $nota->save();
+                }
+
+                            // Obtén todas las notas relacionadas con ese usuario
+                            $notas_relacionadas72 = Paquete2::where('id_user7', $usuario_id)->get();
+
+                            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+                            foreach ($notas_relacionadas72 as $nota) {
+                                $nota->id_user7 = $usuario_reemplazo_id;
+                                $nota->save();
+                            }
+
+                                        // Obtén todas las notas relacionadas con ese usuario
+                                        $notas_relacionadas73 = Paquete2::where('id_user8', $usuario_id)->get();
+
+                                        // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+                                        foreach ($notas_relacionadas73 as $nota) {
+                                            $nota->id_user8 = $usuario_reemplazo_id;
+                                            $nota->save();
+                                        }
+
+            // Obtén todas las notas relacionadas con ese usuario
+            $notas_relacionadas8 = Paquete3::where('id_user9', $usuario_id)->get();
+
+            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+            foreach ($notas_relacionadas8 as $nota) {
+                $nota->id_user9 = $usuario_reemplazo_id;
+                $nota->save();
+            }
+
+                // Obtén todas las notas relacionadas con ese usuario
+                $notas_relacionadas81 = Paquete3::where('id_user10', $usuario_id)->get();
+
+                // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+                foreach ($notas_relacionadas81 as $nota) {
+                    $nota->id_user10 = $usuario_reemplazo_id;
+                    $nota->save();
+                }
+
+                DB::table('horario')
+                ->whereIn('id_user', $usuarios_a_eliminar)
+                ->delete();
+
+                            // Obtén todas las notas relacionadas con ese usuario
+            $notas_relacionadas9 = Alertas::where('id_especialist', $usuario_id)->get();
+
+            // Itera sobre cada nota y reasígnales el ID del nuevo usuario
+            foreach ($notas_relacionadas9 as $nota) {
+                $nota->id_especialist = $usuario_reemplazo_id;
+                $nota->save();
+            }
+            // Elimina el usuario una vez que las notas se han reasignado
+            $usuario_a_eliminar->delete();
+        }
 
         return view('client.index', compact('clients'));
     }
