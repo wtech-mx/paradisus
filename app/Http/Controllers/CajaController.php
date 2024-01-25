39ffba716032112b7ec8dbc677a491b8d26f72ed
verplancken <42614785+verplancken@users.noreply.github.com>
2024-01-24 18:42:49 -0600

    Update CajaController.php

--- app/Http/Controllers/CajaController.php
+++ app/Http/Controllers/CajaController.php
@@ -786,7 +786,7 @@ class CajaController extends Controller
             ->select(DB::raw('SUM(propina) as total'))
             ->first();
 
-            $suma_pago_tarjeta = $servicios_tarjeta->total + $productos_tarjeta->total + $paquete_tarjeta->total + $paquete_tarjeta->total;
+            $suma_pago_tarjeta = $servicios_tarjeta->total + $productos_tarjeta->total + $paquete_tarjeta->total;
             $suma_filas_tarjeta = $servicios_tarjeta->filas + $productos_tarjeta->filas + $paquete_tarjeta->filas;
 
             $total_servicios_tarjeta = Pagos::join('notas', 'pagos.id_nota', '=', 'notas.id')
