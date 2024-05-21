659a7e32bf5606cf07c4f013ef917c16934e3e05
verplancken <42614785+verplancken@users.noreply.github.com>
2024-05-20 18:18:52 -0600

    Update CajaController.php

--- app/Http/Controllers/CajaController.php
+++ app/Http/Controllers/CajaController.php
@@ -571,7 +571,7 @@ class CajaController extends Controller
         //====================================== END GRAFICAS ======================================
 
         $caja_dia_suma_cambios = CajaDia::where('fecha', '=', $diaActual)
-        ->where('concepto', 'LIKE', '%Cambio%')
+        ->where('concepto', 'LIKE', '%Cambio nota%')
         ->where('motivo', '=', 'Retiro')
         ->select(DB::raw('SUM(egresos) as total'))
         ->first();
