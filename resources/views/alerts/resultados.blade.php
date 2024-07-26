a8f0d36e4ba76755e719ae979dd616e332f2f590
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-07-25 19:38:41 -0600

    Vista de rsultado de busqueda user alertas

---
+++ resources/views/alerts/resultados.blade.php
@@ -0,0 +1,28 @@
+<table id="resultadosAlertas" class="table table-flush" id="datatable-search">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Cliente</th>
+            <th>Cosmetologa</th>
+            <th>Fecha Inicio</th>
+            <th>Fecha Fin</th>
+            <th>Estado</th>
+            <th>Servicio</th>
+
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($alertas as $alerta)
+            <tr>
+                <td>{{ $alerta->id }}</td>
+                <td>{{ $alerta->title }}</td>
+                <td>{{ $alerta->cosmetologa }}</td>
+                <td>{!! \Carbon\Carbon::parse($alerta->start)->format('d-m-Y') !!} <br> {!! \Carbon\Carbon::parse($alerta->start)->format('\a \l\a\s h:i A') !!}</td>
+                <td>{!! \Carbon\Carbon::parse($alerta->end)->format('d-m-Y') !!} <br> {!! \Carbon\Carbon::parse($alerta->end)->format('\a \l\a\s h:i A') !!}</td>
+                <td style="background-color: {{ $alerta->status->color }}">{{ $alerta->status->estatus }} <img src="{{ $alerta->status->icono }}" alt="" style="width: 20px"></td>
+                <td>{{ $alerta->service_name  }}</td>
+
+            </tr>
+        @endforeach
+    </tbody>
+</table>
