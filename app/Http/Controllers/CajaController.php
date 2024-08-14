c1e9012ab4145ed7d5497956f89ff2d0eb56340f
verplancken <42614785+verplancken@users.noreply.github.com>
2024-08-13 19:21:43 -0600

    Update CajaController.php

--- app/Http/Controllers/CajaController.php
+++ app/Http/Controllers/CajaController.php
@@ -43,6 +43,13 @@ class CajaController extends Controller
             $caja=$caja->ingresos;
         }
 
+        $propinas = NotasPropinas::where('fecha', '=', $fechaActual)->where('metdodo_pago', '=', 'Efectivo')->get();
+
+        $propinas_suma = NotasPropinas::where('fecha', '=', $fechaActual)
+        ->where('metdodo_pago', '=', 'Efectivo')
+        ->select(DB::raw('SUM(propina) as total'))
+        ->first();
+
         $pago = Pagos::join('notas', 'pagos.id_nota', '=', 'notas.id')
         ->where('pagos.fecha', '=', $fechaActual)
         ->where('pagos.forma_pago', '=', 'Efectivo')
@@ -101,7 +108,7 @@ class CajaController extends Controller
 
         $notas_paquetes = NotasPaquetes::get();
 
-        return view('caja.index', compact('pago_laser','pago_laser_suma','caja_dia_suma_Ingreso','caja_vista','caja', 'pago', 'pago_suma', 'caja_dia', 'caja_dia_suma','pago_pedidos', 'pago_pedidos_suma', 'diaActual', 'notas_paquetes', 'caja', 'pago_paquete', 'pago_paquete_suma'));
+        return view('caja.index', compact('propinas','propinas_suma','pago_laser','pago_laser_suma','caja_dia_suma_Ingreso','caja_vista','caja', 'pago', 'pago_suma', 'caja_dia', 'caja_dia_suma','pago_pedidos', 'pago_pedidos_suma', 'diaActual', 'notas_paquetes', 'caja', 'pago_paquete', 'pago_paquete_suma'));
     }
 
     /**
@@ -251,8 +258,13 @@ class CajaController extends Controller
         ->select(DB::raw('SUM(egresos) as total'))
         ->first();
 
+        $propinas_suma = NotasPropinas::where('fecha', '=', $diaActual)
+        ->where('metdodo_pago', '=', 'Efectivo')
+        ->select(DB::raw('SUM(propina) as total'))
+        ->first();
+
         $total_ing = 0;
-        $total_ing = $caja_dia_suma->total + $pago_suma->total +  $pago_pedidos_suma->total + $pago_paquete_suma->total + $pago_laser_suma->total + $caja_final->ingresos;
+        $total_ing = $caja_dia_suma->total + $propinas_suma->total + $pago_suma->total +  $pago_pedidos_suma->total + $pago_paquete_suma->total + $pago_laser_suma->total + $caja_final->ingresos;
 
         $total_egresos = 0;
         $total_egresos = $total_ing - $caja_dia_resta->total;
@@ -576,12 +588,21 @@ class CajaController extends Controller
         ->select(DB::raw('SUM(egresos) as total'))
         ->first();
 
-        $pdf = \PDF::loadView('caja.pdf_nuevo',['chart' => $chart,'chartmp' => $chartmp], compact('caja_dia_suma_cambios','sumaServiciosEfectivoCambio','suma_pago_tarjeta', 'suma_filas_tarjeta','suma_pago_mercado', 'suma_filas_mercado','suma_pago_trans',
+        $propinas_efectivo = NotasPropinas::where('fecha', '=', $diaActual)->where('metdodo_pago', '=', 'Efectivo')->get();
+        $propinas_tarjeta = NotasPropinas::where('fecha', '=', $diaActual)->where('metdodo_pago', '=', 'Tarjeta')->get();
+        $propinas_transferencia = NotasPropinas::where('fecha', '=', $diaActual)->where('metdodo_pago', '=', 'Transferencia')->get();
+
+        $propinas_suma = NotasPropinas::where('fecha', '=', $diaActual)
+        ->where('metdodo_pago', '=', 'Efectivo')
+        ->select(DB::raw('SUM(propina) as total'))
+        ->first();
+
+        $pdf = \PDF::loadView('caja.pdf_nuevo',['chart' => $chart,'chartmp' => $chartmp], compact('propinas_efectivo', 'propinas_tarjeta', 'propinas_transferencia', 'propinas_suma','caja_dia_suma_cambios','sumaServiciosEfectivoCambio','suma_pago_tarjeta', 'suma_filas_tarjeta','suma_pago_mercado', 'suma_filas_mercado','suma_pago_trans',
         'suma_filas_trans','propinasHoy','caja_rep','paquetes','today', 'caja', 'servicios', 'productos_rep', 'caja_dia_suma', 'notas_paquetes',
         'total_servicios_trans', 'total_servicios_mercado', 'total_servicios_tarjeta', 'total_producto_trans', 'total_producto_mercado', 'total_producto_tarjeta',
         'total_paquetes_trans', 'total_paquetes_mercado', 'total_paquetes_tarjeta','bitacora', 'notas_laser', 'total_laser_trans', 'total_laser_mercado', 'total_laser_tarjeta', 'caja_dia_suma_vista'));
-         //return $pdf->stream();
-        return $pdf->download('Reporte Caja '.$today.'.pdf');
+         return $pdf->stream();
+       //  return $pdf->download('Reporte Caja '.$today.'.pdf');
     }
 
     public function imprimir_precorte(){
@@ -628,6 +649,11 @@ class CajaController extends Controller
             ) AS total'))
             ->first();
 
+            $propinas_suma = NotasPropinas::where('fecha', '=', $diaActual)
+            ->where('metdodo_pago', '=', 'Efectivo')
+            ->select(DB::raw('SUM(propina) as total'))
+            ->first();
+
             $pago_paquete_suma = PaquetesPago::where('fecha', '=', $diaActual)
             ->where('forma_pago', '=', 'Efectivo')
             ->select(DB::raw('SUM(dinero_recibido) as total'))
@@ -661,9 +687,9 @@ class CajaController extends Controller
             ->first();
 
             $total_ing = 0;
-            $total_ing = $caja_dia_suma->total + $pago_suma->total +  $pago_pedidos_suma->total + $pago_paquete_suma->total + $pago_laser_suma->total + $caja_final->ingresos;
+            $total_ing = $caja_dia_suma->total + $pago_suma->total +  $pago_pedidos_suma->total + $pago_paquete_suma->total + $pago_laser_suma->total + $caja_final->ingresos + $propinas_suma->total;
             $total_ing_vista = 0;
-            $total_ing_vista =  $pago_suma->total +  $pago_pedidos_suma->total + $pago_paquete_suma->total + $pago_laser_suma->total + $caja_final->ingresos;
+            $total_ing_vista =  $pago_suma->total +  $pago_pedidos_suma->total + $pago_paquete_suma->total + $pago_laser_suma->total;
 
             $total_egresos = 0;
             $total_egresos = $total_ing - $caja_dia_resta->total;
@@ -735,12 +761,12 @@ class CajaController extends Controller
             ->select(DB::raw('SUM(dinero_recibido) as total'), DB::raw('count(*) as filas'))
             ->first();
 
-            $propinasTrans = NotasPropinas::whereDate('created_at', $diaActual)
+            $propinasTrans = NotasPropinas::whereDate('fecha', $diaActual)
             ->where('metdodo_pago', '=', 'Transferencia')
             ->select(DB::raw('SUM(propina) as total'))
             ->first();
 
-            $suma_pago_trans = $servicios_trans->total + $productos_trans->total + $paquete_trans->total + $laser_trans->total;
+            $suma_pago_trans = $servicios_trans->total + $productos_trans->total + $paquete_trans->total + $laser_trans->total + $propinasTrans->total;
             $suma_filas_trans = $servicios_trans->filas + $productos_trans->filas + $paquete_trans->filas + $laser_trans->filas;
 
             $total_servicios_trans = Pagos::join('notas', 'pagos.id_nota', '=', 'notas.id')
@@ -863,7 +889,7 @@ class CajaController extends Controller
             ->select(DB::raw('SUM(dinero_recibido) as total'), DB::raw('count(*) as filas'))
             ->first();
 
-            $propinasTarjeta = NotasPropinas::whereDate('created_at', $diaActual)
+            $propinasTarjeta = NotasPropinas::whereDate('fecha', $diaActual)
             ->where('metdodo_pago', '=', 'Tarjeta')
             ->select(DB::raw('SUM(propina) as total'))
             ->first();
@@ -886,12 +912,15 @@ class CajaController extends Controller
             $total_paquetes_tarjeta = PaquetesPago::where('fecha', '=', $diaActual)->where('forma_pago', '=', 'Tarjeta')->get();
         //====================================== END TOTALES PARA TARJETA ======================================
 
+        $propinas_efectivo = NotasPropinas::where('fecha', '=', $diaActual)->where('metdodo_pago', '=', 'Efectivo')->get();
+        $propinas_tarjeta = NotasPropinas::where('fecha', '=', $diaActual)->where('metdodo_pago', '=', 'Tarjeta')->get();
+        $propinas_transferencia = NotasPropinas::where('fecha', '=', $diaActual)->where('metdodo_pago', '=', 'Transferencia')->get();
 
-        $pdf = \PDF::loadView('caja.precorte', compact('total_ing_vista','notas_laser','total_laser_trans','total_laser_mercado','total_laser_tarjeta','pago_laser','caja_dia_suma_cambios','sumaServiciosEfectivoCambio','suma_pago_tarjeta', 'suma_filas_tarjeta',
+        $pdf = \PDF::loadView('caja.precorte', compact('propinas_efectivo','propinas_tarjeta','propinas_transferencia', 'propinas_suma','total_ing_vista','notas_laser','total_laser_trans','total_laser_mercado','total_laser_tarjeta','pago_laser','caja_dia_suma_cambios','sumaServiciosEfectivoCambio','suma_pago_tarjeta', 'suma_filas_tarjeta',
         'suma_pago_mercado', 'suma_filas_mercado','suma_pago_trans', 'caja_final','suma_filas_trans','propinasHoy','total_ing','caja_egre','total_egresos','paquetes',
         'fechaYHoraFormateada', 'caja', 'servicios', 'productos_rep', 'caja_dia_suma', 'notas_paquetes','total_servicios_trans', 'total_servicios_mercado', 'total_servicios_tarjeta',
-        'total_producto_trans', 'total_producto_mercado', 'total_producto_tarjeta','total_paquetes_trans', 'total_paquetes_mercado', 'total_paquetes_tarjeta','bitacora'));
-        // return $pdf->stream();
+        'total_producto_trans', 'total_producto_mercado', 'total_producto_tarjeta','total_paquetes_trans', 'total_paquetes_mercado', 'total_paquetes_tarjeta','bitacora', 'caja_dia_suma_vista'));
+         //return $pdf->stream();
         return $pdf->download('Precorte '.$fechaYHoraFormateada.'.pdf');
     }
 }
