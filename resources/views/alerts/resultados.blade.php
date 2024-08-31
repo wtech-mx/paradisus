bee9c74bf38f0d3b770f2e622b1fa45f5a9f90ee
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-08-30 19:38:56 -0600

    Actuzalaicion de tmañao de tabla

--- resources/views/alerts/resultados.blade.php
+++ resources/views/alerts/resultados.blade.php
@@ -13,17 +13,53 @@
     </thead>
     <tbody>
         @foreach($alertas as $alerta)
+        @php
+            $words = explode(' ', $alerta->title);
+            $formattedTitle = '';
+            foreach($words as $index => $word) {
+                $formattedTitle .= $word;
+                if (($index + 1) % 2 == 0 && $index < count($words) - 1) {
+                    $formattedTitle .= '<br>';
+                } else {
+                    $formattedTitle .= ' ';
+                }
+            }
+
+            $words = explode(' ', $alerta->cosmetologa);
+            $formattedTitleCosme = '';
+            foreach($words as $index => $word) {
+                $formattedTitleCosme .= $word;
+                if (($index + 1) % 2 == 0 && $index < count($words) - 1) {
+                    $formattedTitleCosme .= '<br>';
+                } else {
+                    $formattedTitleCosme .= ' ';
+                }
+            }
+
+            $words = explode(' ', $alerta->service_name);
+            $formattedTitleServicio = '';
+            foreach($words as $index => $word) {
+                $formattedTitleServicio .= $word;
+                if (($index + 1) % 2 == 0 && $index < count($words) - 1) {
+                    $formattedTitleServicio .= '<br>';
+                } else {
+                    $formattedTitleServicio .= ' ';
+                }
+            }
+
+
+        @endphp
             <tr>
                 <td>{{ $alerta->id }}</td>
-                <td>{{ $alerta->title }}</td>
-                <td>{{ $alerta->cosmetologa }}</td>
+                <td>{!! $formattedTitle !!}</td>
+                <td>{!! $formattedTitleCosme !!}</td>
                 <td>{!! \Carbon\Carbon::parse($alerta->start)->format('d-m-Y') !!} <br> {!! \Carbon\Carbon::parse($alerta->start)->format('\a \l\a\s h:i A') !!}</td>
                 <td>{!! \Carbon\Carbon::parse($alerta->end)->format('d-m-Y') !!} <br> {!! \Carbon\Carbon::parse($alerta->end)->format('\a \l\a\s h:i A') !!}</td>
                 <td style="background-color: {{ $alerta->status->color }}">{{ $alerta->status->estatus }} <img src="{{ $alerta->status->icono }}" alt="" style="width: 20px"></td>
-                <td>{{ $alerta->service_name  }}</td>
+                <td>{!! $formattedTitleServicio !!}</td>
                 <td>
 
-                    <button type="button" class="btn btn-primary modal-trigger" data-target="#modal-{{ $alerta->id }}">
+                    <button type="button" class="btn btn-xs btn-primary modal-trigger" data-target="#modal-{{ $alerta->id }}">
                         Aprobar
                     </button>
 
