657597ca03c649a6c7ee56776abdf40a9c1a0cce
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-09-26 14:06:51 -0600

    ultimos 100 registros

--- app/Http/Controllers/NotasPedidoController.php
+++ app/Http/Controllers/NotasPedidoController.php
@@ -34,7 +34,7 @@ class NotasPedidoController extends Controller
      */
     public function index()
     {
-        $nota_pedido = NotasPedidos::with('pedidos')->orderBy('id', 'DESC')->take(500)->get();
+        $nota_pedido = NotasPedidos::with('pedidos')->orderBy('id', 'DESC')->take(100)->get();
         // $nota_pedido = NotasPedidos::orderBy('id','DESC')->get();
         return view('notas_pedidos.index', compact('nota_pedido'));
     }
