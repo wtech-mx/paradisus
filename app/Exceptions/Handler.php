edec0ef7dd74199f026899fbb2966b94aafc7f74
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-12-04 14:35:58 -0600

    Actuzalaciuon de envio de correo reporte d

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Support\Facades\Mail;
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +38,52 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function report(Throwable $exception)
+    {
+        if ($this->shouldReport($exception)) {
+            $message = $exception->getMessage();
+            $statusCode = method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 500;
+            $subject = "Error $statusCode: $message";
+
+            if (app()->bound('sentry') && $this->shouldReportToSentry($exception)) {
+                app('sentry')->captureException($exception);
+            }
+
+            if (app()->environment('production')) {
+                $this->sendEmail($subject, $exception);
+            }
+
+            // Almacenar el error en la sesión para pasarlo a la vista
+            session()->flash('error_code', $statusCode);
+            session()->flash('error_message', $message);
+            session()->flash('error_trace', $exception->getTraceAsString());
+        }
+
+        parent::report($exception);
+    }
+
+    protected function sendEmail($subject, $exception)
+    {
+
+        $to = 'adrianwebtech@gmail.com';
+        $to2  = 'dayanna.wtech@gmail.com';
+
+        $message = $exception->getMessage() . "\n\n" .
+            $exception->getTraceAsString() . "\n\n" .
+            'Request Data: ' . json_encode(request()->all());
+
+        // Mail::raw($message, function ($email) use ($to, $subject) {
+        //     $email->to($to)
+        //           ->subject($subject);
+        // });
+
+        Mail::raw($message, function ($email) use ($to, $to2, $subject) {
+            $email->to($to)
+                  ->to($to2)
+                  ->subject($subject);
+        });
+
     }
 }
